fb3203a386ef854c2b0c2e105eb5dc5e9761291e
ZENHOM <you@example.com>
2023-06-30 20:02:11 +0300

    eslintrc update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,13 +21,6 @@ const config = {
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
