4478275b1f54b5888d2e61031e99e925acf3dbe1
ZENHOM <you@example.com>
2023-06-30 20:40:59 +0300

    nageny ya allah

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,8 +21,18 @@ const config = {
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@next/next/no-html-link-for-pages": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
     "react/jsx-key": "off",
   },
   settings: {
