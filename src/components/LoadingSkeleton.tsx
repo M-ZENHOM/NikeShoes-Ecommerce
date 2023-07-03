6cb2fe8179673590dae591acaeaf57b69b0326d9
ZENHOM <you@example.com>
2023-07-03 23:44:43 +0300

    skeleton style

--- src/components/LoadingSkeleton.tsx
+++ src/components/LoadingSkeleton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LoadingSkeleton = () => {
     return (
-        <div className='flex flex-col justify-center items-center h-[65vh]'>
+        <div className='flex  justify-center items-center h-[65vh]'>
             <span className="loading loading-bars loading-xs"></span>
             <span className="loading loading-bars loading-sm"></span>
             <span className="loading loading-bars loading-md"></span>
